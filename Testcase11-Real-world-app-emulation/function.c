474084f269c454f751147966c7cf3aee710469d4
CodingYuanLiu <lqyuan980413@163.com>
2020-12-04 10:45:48 +0800

    Add license

--- Testcase11-Real-world-app-emulation/function.c
+++ Testcase11-Real-world-app-emulation/function.c
@@ -1,3 +1,13 @@
+// Copyright (c) 2020 Institution of Parallel and Distributed System, Shanghai Jiao Tong University
+// ServerlessBench is licensed under the Mulan PSL v1.
+// You can use this software according to the terms and conditions of the Mulan PSL v1.
+// You may obtain a copy of Mulan PSL v1 at:
+//     http://license.coscl.org.cn/MulanPSL
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+// PURPOSE.
+// See the Mulan PSL v1 for more details.
+
 // This is a simplest function in a function chain
 #include <stdio.h>
 #include <stdlib.h>
@@ -35,7 +45,8 @@ char* GeneratePlaceholder(){
 
 
 int main(int argc, char* argv[]){
-    struct timeval tv;
+    struct timeval tv, endtv;
+    long startTime, endTime;
     int allocMemSize;
     gettimeofday(&tv, NULL);
     if(argc < 2){
@@ -44,10 +55,16 @@ int main(int argc, char* argv[]){
     }
     allocMemSize = atoi(argv[1]);
     AllocMem(allocMemSize);
+    gettimeofday(&endtv, NULL);
+    startTime = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+    endTime = endtv.tv_sec * 1000 + endtv.tv_usec / 1000;
     printf(
         "{\n" 
-        "   \"startTime\": %ld\n"
-        "}\n", tv.tv_sec * 1000 + tv.tv_usec / 1000
+        "   \"startTime\": %ld,\n"
+        "   \"execTime\": %ld\n"
+        "}\n", 
+        startTime,
+        endTime - startTime
     );
     return 0;
 }
